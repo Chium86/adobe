64de8f0002a8bfa8cded3947175fe63cb85333e9
Chium86 <ccy.design317@gmail.com>
2024-06-26 19:26:38 +0800

    fancybox tool

--- js/master.js
+++ js/master.js
@@ -3,42 +3,76 @@ const isMobile = $(window).width() <= 480;  //抓出可視範圍寬度,並判斷
 const $grid = $('#Grid');
 const $nav = $('#Nav');
 
-function setGrid(){
-    if (isMobile){
+function setGrid() {
+    if (isMobile) {
         $grid.addClass('grid-2');
         return;
     }
-        $grid.addClass('grid-3');
+    $grid.addClass('grid-3');
 }
 
-function setScroll(){
-    $(window).scroll(function() {
-    // isMobile = true
-    // !isMobile = false
-    if (!isMobile) return;
-    
-    // console.log($(window).scrollTop());
-    if($(window).scrollTop() == 0){
-        $nav.removeClass('nav-active');
-    }else{
-        $nav.addClass('nav-active');
-    }    
-});
+function setScroll() {
+    $(window).scroll(function () {
+        // isMobile = true
+        // !isMobile = false
+        if (!isMobile) return;
+
+        // console.log($(window).scrollTop());
+        if ($(window).scrollTop() == 0) {
+            $nav.removeClass('nav-active');
+        } else {
+            $nav.addClass('nav-active');
+        }
+    });
 }
 
-function setInit(){
+function setInit() {
     setGrid(); //呼叫
 }
 
-function setEvent(){
+function setEvent() {
     setScroll();
     setFancybox();
 }
 
-function setFancybox(){
+function setFancybox() {
     $grid.find('a').fancybox({
-        protect:true,
-        loop:true,
+        protect: true,
+        loop: true,
+        animationDuration: 400,
+        transitionDuration: 1000,
+        transitionEffect: 'circular',
+        // fullScreen:
+        // autoStart:true,
+        // }
+        // keyboard:false,
+        infobar: false,
+        // toolbar:false,
+        // smallBtn:true,
+        // arrows:false,
+        // slideShow:{
+        // autoStart:true
+        // speed:1000
+        //         }
+        thumbs: false,
+        
+        buttons: [
+            'share',
+            'slideShow',
+            'fullScreen',
+            'download',
+            'thumbs',
+            'close',
+        ],
+        
+        media:{
+            youtube:{
+                params:{
+                    autoplay:false
+                }
+            },
+
+        }
     });
 }
 
